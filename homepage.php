e34d96049cb975e860cd1bde0457e803cbabc182
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2023-02-04 17:43:50 +0100

    Fix home dummy photo

--- homepage.php
+++ homepage.php
@@ -25,7 +25,7 @@
       <p></p>
     </div>
     <div class="col-md-7">
-      <img src="././img/scuola-vela.jpg" class="img-fluid" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?> /img/scuola-vela.jpg" class="img-fluid" alt="">
 
     </div>
   </div>
