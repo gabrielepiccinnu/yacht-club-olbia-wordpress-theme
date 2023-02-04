e1265f012a6179a48f571c648ad54abc67fde5d8
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2023-02-04 17:40:05 +0100

    Dummy content added

--- homepage.php
+++ homepage.php
@@ -10,4 +10,30 @@
 <?php get_template_part( 'template-parts/content', 'homepage-slider' ); ?>
 
 
+
+<!-- DUMMY CONTENT -->
+
+<div class="container py-5">
+  <div class="row align-items-center">
+    <div class="col-md-5 pe-lg-5">
+      <p class="section-pretitle mb-2">Yacht Club Olbia</p>
+      <h2 class="h1 mb-5">Scuola di Vela</h2>
+      <p>La scuola vela racchiude il vero scopo dello Yacht Club Olbia.</p>
+      <p>Il primo obiettivo che il Club si prefigge è proprio quello di permettere a chiunque lo desideri di saper andar per mare … a vela!</p>
+      <p></p>Corollario nobile di questo progetto è diffondere lo sport della vela e della cultura del mare, con le sue regole, per poterlo meglio affrontare nel rispetto dei valori etici e delle sane abitudini di vita legate allo sport, ma anche con la necessaria conoscenza degli elementi che ci circondano e con la fantasia nel trovare una strada sempre diversa, cercando di migliorarsi.<p></p>
+      <p>Non sono secondari il rispetto dei regolamenti e degli avversari e la conoscenza dei propri limiti che possono offrire un determinante supporto all’educazione e allo sviluppo della persona</p>
+      <p></p>
+    </div>
+    <div class="col-md-7">
+      <img src="././img/scuola-vela.jpg" class="img-fluid" alt="">
+
+    </div>
+  </div>
+</div>
+
+
+<!-- DUMMY CONTENT -->
+
+
+
 <?php get_footer(); ?>
