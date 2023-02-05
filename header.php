fe5afa091eec32eafcba5b087f83c437216fff27
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2023-02-06 00:24:43 +0100

    Fixed link brand header

--- header.php
+++ header.php
@@ -76,7 +76,7 @@
 
     <nav class="navbar navbar-dark navbar-expand-lg navbar-main-yco bg-primary">
       <div class="container">
-        <a class="navbar-brand" href="#"><span class="yachtclubolbia-logo"></span></a>
+        <a class="navbar-brand" href="<?php echo get_home_url(); ?> "><span class="yachtclubolbia-logo"></span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
