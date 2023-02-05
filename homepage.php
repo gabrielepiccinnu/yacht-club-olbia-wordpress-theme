82c5f5cb71f04d93f307579fbb0df087a57bc5a2
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2023-02-05 16:08:07 +0100

    homepage.php with Gutenberg support

--- homepage.php
+++ homepage.php
@@ -34,6 +34,22 @@
 
 <!-- DUMMY CONTENT -->
 
+<section>
+<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'homepage' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+</section>
+
 
 
 <?php get_footer(); ?>
