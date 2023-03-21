f27a51b2b4fb6f275cf9e7f6e5a0929e11ff7f7d
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2023-03-21 19:40:58 +0100

    content page contact us template page

---
+++ template-parts/content-page-contactus.php
@@ -0,0 +1,19 @@
+
+<section class="bg-yco-light-gradient py-5">
+    <div class="container py-5">
+      <div class="row g-5">
+        <div class="col-12 col-md-6 col-lg-4  offset-lg-2">
+          <p class="text-primary text-uppercase fw-semibold">Yacht Club Olbia</p>
+          <p class="h2 mb-3">Hai domande?<br>Contattaci qui</p>
+          <p>Compila il seguente modulo di contatto per ricevere maggiori informazioni.</p>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <?php echo do_shortcode('[contact-form-7 id="71" title="Form pagina contatti"]'); ?>
+           
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <?php 
